af51e7f8b2619b8ec24f1d812c2f37bc7e5b19fd
Reva Mahto <reva.mahto@gmail.com>
2024-03-05 18:29:31 -0800

    update 1

---
+++ ForecastViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ForecastViewController.swift
+//  CloudCast
+//
+//  Created by Reva Mahto on 3/5/24.
+//
+
+import UIKit
+
+class ForecastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
